e1d3922655d8d77b327dbdb5637c20373a48ec9c
ananyateklu <teklu009@morris.umn.edu>
2019-11-06 21:39:29 -0600

    implemented ThreadedSearch

--- src/search/ThreadedSearch.java
+++ src/search/ThreadedSearch.java
@@ -28,30 +28,27 @@ public class ThreadedSearch<T> implements Searcher<T>, Runnable {
      * <p>
      * You can assume that the list size is divisible by `numThreads`
      */
+    @Override
     public boolean search(T target, List<T> list) throws InterruptedException {
-        /*
-         * First construct an instance of the `Answer` inner class. This will
-         * be how the threads you're about to create will "communicate". They
-         * will all have access to this one shared instance of `Answer`, where
-         * they can update the `answer` field inside that instance.
-         *
-         * Then construct `numThreads` instances of this class (`ThreadedSearch`)
-         * using the 5 argument constructor for the class. You'll hand each of
-         * them the same `target`, `list`, and `answer`. What will be different
-         * about each instance is their `begin` and `end` values, which you'll
-         * use to give each instance the "job" of searching a different segment
-         * of the list. If, for example, the list has length 100 and you have
-         * 4 threads, you would give the four threads the ranges [0, 25), [25, 50),
-         * [50, 75), and [75, 100) as their sections to search.
-         *
-         * You then construct `numThreads`, each of which is given a different
-         * instance of this class as its `Runnable`. Then start each of those
-         * threads, wait for them to all terminate, and then return the answer
-         * in the shared `Answer` instance.
-         */
-        return false;
+     
+        Answer answer = new Answer();
+        //create arrays that hold the threadSearch
+        Thread[] threads = new Thread[numThreads];
+        for(int i=0; i<numThreads; i++) {
+            int begin = (list.size()*i)/ numThreads;
+            int end = (list.size()*(i+1))/ numThreads;
+            ThreadedSearch<T> threadedSearch = new ThreadedSearch<T>(target, list, begin,end,answer);
+            threads[i] = new Thread(threadedSearch);
+            threads[i].start();
+        }  
+
+        for(int i = 0; i < numThreads; i++){
+            threads[i].join();
+        }
+        return answer.getAnswer();
     }
 
+    @Override
     public void run() {
         // Delete this `throw` when you actually implement this method.
         throw new UnsupportedOperationException();
